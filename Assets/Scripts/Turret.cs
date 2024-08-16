4e929a69421e984abfe80a0ac88f354d108119aa
lotuuu <nicolas@lotufo.com.ar>
2024-08-16 15:59:39 -0300

    Add basic fire code

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,8 +5,11 @@ public class Turret : MonoBehaviour
 {
     Vector2 moveInput;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float fireCooldown = 5f;
 
     new Rigidbody2D rigidbody2D;
+
+    float fireTimer = 0f;
 
     void Start()
     {
@@ -15,8 +18,29 @@ public class Turret : MonoBehaviour
 
     void FixedUpdate()
     {
+        HandleMovement();
+        HandleFire();
+    }
+
+    void HandleMovement()
+    {
         Vector2 newVelocity = new(moveInput.x * moveSpeed, rigidbody2D.velocity.y);
         rigidbody2D.velocity = newVelocity;
+    }
+
+    void HandleFire()
+    {
+        fireTimer -= Time.deltaTime;
+        if (fireTimer <= 0f)
+        {
+            Fire();
+            fireTimer = fireCooldown;
+        }
+    }
+
+    void Fire()
+    {
+        Debug.Log("Firing!");
     }
 
     void OnMove(InputValue inputValue)
